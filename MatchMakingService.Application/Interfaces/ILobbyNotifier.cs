3ad7898a711fb4ab11b350cb80e7b583568a4014
Sujith Nair <sujithaturservice@gmail.com>
2026-02-12 17:33:37 +0530

    Implemented Lobby Notifier

--- MatchMakingService.Application/Interfaces/ILobbyNotifier.cs
+++ MatchMakingService.Application/Interfaces/ILobbyNotifier.cs
@@ -8,7 +8,7 @@ public interface ILobbyNotifier
     public Task NotifyLobbyCreationFailedAsync(string connectionId, string errorMessage);
     public Task NotifyLobbyJoinedAsync(string connectionId, string lobbyCode, string lobbyName, List<Player> currentPlayers);
     public Task NotifyLobbyJoinFailedAsync(string connectionId, string errorMessage);
-    public Task NotifyPlayerJoinedAsync(string lobbyCode, List<Player> currentPlayers);
+    public Task NotifyPlayerJoinedAsync(string lobbyCode, string connectionIdOfPlayerJoined, List<Player> currentPlayers);
     public Task NotifyPlayerLeftAsync(string lobbyCode, string connectionIdOfPlayerLeft, List<Player> currentPlayers);
     public Task NotifyLobbyLockedAsync(string lobbyCode, List<Player> currentPlayers);
 }
--- MatchMakingService/Hubs/LobbyNotifier.cs
+++ MatchMakingService/Hubs/LobbyNotifier.cs
@@ -1,49 +1,51 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using MatchMakingService.Domain.Entities;
+using MatchMakingService.Application.Constants;
 using MatchMakingService.Application.Interfaces;
 
 namespace MatchMakingService.Hubs;
 
-public class LobbyNotifier : ILobbyNotifier
+public class LobbyNotifier(IHubContext<LobbyHub> hubContext, ILogger<LobbyNotifier> logger)
+    : ILobbyNotifier
 {
-    public LobbyNotifier(IHubContext<LobbyHub> hubContext, ILogger<LobbyNotifier> logger)
-    {
-        
-    }
+    private IGroupManager GroupManager => hubContext.Groups;
+    private IHubClients Clients => hubContext.Clients;
     
-    public Task NotifyLobbyCreatedAsync(string connectionId, string lobbyCode, string lobbyName)
+    public async Task NotifyLobbyCreatedAsync(string connectionId, string lobbyCode, string lobbyName)
+    {
+        await GroupManager.AddToGroupAsync(connectionId, lobbyCode);
+        await Clients.Client(connectionId).SendAsync(SignalRConstants.OnLobbyCreated, lobbyCode, lobbyName);
+    }
+
+    public async Task NotifyLobbyCreationFailedAsync(string connectionId, string errorMessage)
+    {
+        await Clients.Client(connectionId).SendAsync(SignalRConstants.OnLobbyCreationFailed, errorMessage);
+    }
+
+    public async Task NotifyLobbyJoinedAsync(string connectionId, string lobbyCode, string lobbyName, List<Player> currentPlayers)
+    {
+        await GroupManager.AddToGroupAsync(connectionId, lobbyCode);
+        await Clients.Client(connectionId).SendAsync(SignalRConstants.OnLobbyJoined, lobbyCode, lobbyName, currentPlayers);
+        await NotifyPlayerJoinedAsync(lobbyCode, connectionId, currentPlayers);
+    }
+
+    public async Task NotifyLobbyJoinFailedAsync(string connectionId, string errorMessage)
+    {
+        await Clients.Client(connectionId).SendAsync(SignalRConstants.OnLobbyJoinFailed, errorMessage);
+    }
+
+    public async Task NotifyPlayerJoinedAsync(string lobbyCode, string connectionIdOfPlayerJoined, List<Player> currentPlayers)
+    {
+        await Clients.GroupExcept(lobbyCode, connectionIdOfPlayerJoined).SendAsync(SignalRConstants.OnPlayerJoined, currentPlayers);
+    }
+
+    public async Task NotifyPlayerLeftAsync(string lobbyCode, string connectionIdOfPlayerLeft, List<Player> currentPlayers)
     {
         throw new NotImplementedException();
     }
 
-    public Task NotifyLobbyCreationFailedAsync(string connectionId, string errorMessage)
+    public async Task NotifyLobbyLockedAsync(string lobbyCode, List<Player> currentPlayers)
     {
-        throw new NotImplementedException();
-    }
-
-    public Task NotifyLobbyJoinedAsync(string connectionId, string lobbyCode, string lobbyName, int currentPlayers,
-        int maxPlayers)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task NotifyLobbyJoinFailedAsync(string connectionId, string errorMessage)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task NotifyPlayerJoinedAsync(string lobbyCode, List<Player> currentPlayers)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task NotifyPlayerLeftAsync(string lobbyCode, string connectionIdOfPlayerLeft, List<Player> currentPlayers)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task NotifyLobbyLockedAsync(string lobbyCode, List<Player> currentPlayers)
-    {
-        throw new NotImplementedException();
+        await Clients.Group(lobbyCode).SendAsync(SignalRConstants.OnLobbyLocked, currentPlayers);
     }
 }
